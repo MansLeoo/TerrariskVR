fc1a0bca1a4062d5c78eb17d09501cd70a7c571f
MansLeo <leo.mans@ensam.eu>
2026-01-19 12:04:14 +0100

    Ajout du menu

---
+++ Assets/Script/UI/UiManager.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+
+    public static UiManager Instance { get; private set; }
+
+
+    [SerializeField] private GameObject InformationPage;
+    [SerializeField] private GameObject PrincipalePage;
+    private void Awake()
+    {
+        // Logique Singleton : S'assurer qu'il n'y a qu'un seul UIManager
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AfficherInformation()
+    {
+        PrincipalePage.SetActive(false);
+        InformationPage.SetActive(true);
+
+    }
+    public void AfficherPrincipale()
+    {
+        PrincipalePage.SetActive(false);
+        InformationPage.SetActive(false);
+    }
+}
